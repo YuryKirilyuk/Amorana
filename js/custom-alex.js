5df87eca316a22613b450ffe1bc5d4ed9f3cb42d
Alexandr <alexandrdrd@gmail.com>
2019-09-04 15:55:22 +0300

    Hotspot changes

--- js/custom-alex.js
+++ js/custom-alex.js
@@ -1,6 +1,9 @@
 $(document).ready(function(){
 
     if($('.js-dots').length > 0){
+        let currentDot,
+            dotsLength;
+
         $('.js-showDot').on('click', function(e){
             e.preventDefault();
 
@@ -10,6 +13,8 @@ $(document).ready(function(){
                 dotBg = $('.js-plansTrBg'),
                 dHeight = dots.height(),
                 dWidth = dots.width();
+
+            currentDot = dot;
 
             let dotBgX = -((6000 / 2) - (dWidth / 2) - (el.offset().left - (dWidth / 2)) - (el.outerWidth() / 2)),
                 dotBgY = -((3000 / 2) - (dHeight / 2) - (el.offset().top - dots.offset().top - (dHeight / 2)) - (el.outerHeight() / 2));
@@ -37,6 +42,43 @@ $(document).ready(function(){
 
             dots.find('.js-dotsItem').removeClass('active');
             dots.removeClass('active');
+
+        }).on('click', '.js-dotNav', function(e){
+
+            e.preventDefault();
+
+            const dotsList = $(this).closest('.js-dotsList');
+            const dot = dotsList.find('.js-dotsItem');
+
+            let dir = $(this).data('nav'),
+                currDotI = currentDot.index();
+
+            dotsLength = dotsList.find('.js-dotsItem').length;
+
+            dot.removeClass('active');
+
+            if(dir === 'next'){
+                if(currDotI < dotsLength-1){
+                    dot.eq(currDotI + 1).find('.js-showDot').click();
+                } else {
+                    dot.eq(0).find('.js-showDot').click();
+                }
+            } else {
+                if(currDotI === 0){
+                    dot.eq(dotsLength-1).find('.js-showDot').click();
+                } else {
+                    dot.eq(currDotI-1).find('.js-showDot').click();
+                }
+            }
+        });
+
+        $('body').click(function (event){
+            const dots = $('.js-dots');
+
+            if(!$(event.target).closest('.js-dotsItem').length && !$(event.target).is('.js-dotsItem')) {
+                dots.find('.js-dotsItem').removeClass('active');
+                dots.removeClass('active');
+            }
         });
     }
 
